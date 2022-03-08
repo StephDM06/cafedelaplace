599c9595b2a9288480120913bdc4113ccdc24ef7
juanmalisa <juanmalisa@gmail.com>
2022-03-08 11:01:31 +0100

    commitJuan11am

--- script.js
+++ script.js
@@ -1,52 +1,57 @@
 let contenuProd = document.querySelector("#saisie");
+// let dataProd = document.querySelector(".typeProduit");
 let degre = document.querySelector("#degre");
 let detailProduit = document.querySelector(".accordion-body");
 let produittab = [];
 let validProduit = document.querySelector("#validNewProd");
 let product;
 
+// contenuProd.style.visibility = "hidden";
+
 let dataQuery = document.querySelector("#detailProd");
+
+let data = new FormData(dataQuery);
+let nom = data.get("nom");
+let paht = data.get("paht");
+let marge = data.get("marge");
+let degreAlcool = data.get("degre");
+let quantite = data.get("quantite");
 
 dataQuery.addEventListener("submit", function (e) {
   e.preventDefault();
-  // let data = new FormData(dataQuery);
-  // console.log(data.get("selectType"));
-
-  remplirFormulaire();
-});
-
-contenuProd.style.visibility = "hidden";
-
-function remplirFormulaire() {
-  let data = new FormData(dataQuery);
-  let nom = data.get("nom");
-  let paht = data.get("paht");
-  let marge = data.get("marge");
-  let quantite = data.get("quantitestock");
-  let degreAlcool = data.get("degre");
+  console.log(data.get("selectType"));
 
   if (data.get("selectType") == "autre") {
     contenuProd.style.visibility = "visible";
     degre.style.display = "none";
-
     product = new ProtoProduit(nom, paht, marge, quantite);
-
     console.log("sansAlcool");
   } else {
     contenuProd.style.visibility = "visible";
-
     product = new ProtoBalcool(nom, paht, marge, quantite, degreAlcool);
-
     console.log("avecAlcool");
   }
+
   validProduit.addEventListener("click", function () {
     console.log("J'ai validé");
-
     produittab.push(product);
     localStorage.setItem(`@produit `, JSON.stringify(produittab));
     console.log(product);
   });
-}
+});
+
+contenuProd.style.visibility = "hidden";
+// function produitSupprime() {
+//   let boutonSupprime = document.querySelector(".boutonSupprime");
+
+//   boutonSupprime.Array.forEach((element) => {});
+//   (function (boutonSupprimes, index) {
+//     boutonSupprimes.addEventListener("click", function () {
+//       produittab.splice(index, 1);
+//       ajoutProduit();
+//     });
+//   });
+// }
 
 /**
  *
